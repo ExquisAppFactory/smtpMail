bbabdb6cd8a623ec2f9e3f210ddb9e9e05c55f03
Osagie Erhabor <erhaborosagie@gmail.com>
2020-08-14 08:26:51 +0100

    Initial Commit

---
+++ src/main/java/com/osagieerhabor/smtpMail/Providers/Gmail.java
@@ -0,0 +1,11 @@
+package com.osagieerhabor.smtpMail.Providers;
+
+import org.springframework.stereotype.Service;
+
+/*
+    https://developers.google.com/gmail/api/guides/sending
+    https://developers.google.com/gmail/api/quickstart/java
+ */
+@Service
+public class Gmail {
+}
